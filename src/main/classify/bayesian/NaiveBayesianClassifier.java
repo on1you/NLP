8ae24f2c7e4b4a7c8949c02dc3114a25f6c69989
on1you <liuxueping@172.16.0.129>
2013-11-26 11:46:02 +0800

    去掉对训练集目录的依赖，只需读取模型文件进行解析即可分类

--- src/main/classify/bayesian/NaiveBayesianClassifier.java
+++ src/main/classify/bayesian/NaiveBayesianClassifier.java
@@ -1,22 +1,14 @@
 ﻿package main.classify.bayesian;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
@@ -24,46 +16,38 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.Vector;
 
-import org.ansj.util.FilterModifWord;
-import org.apache.commons.io.FileUtils;
-
 import love.cq.util.StringUtil;
-import main.classify.ClassifierMain;
 import main.classify.common.Constants;
 import main.classify.common.TrainModel;
-import main.classify.common.CreateTrainAndTestSample;
-import main.treesplit.util.IOUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * 利用朴素贝叶斯算法对newsgroup文档集做分类，采用十组交叉测试取平均值
- * 采用多项式模型,stanford信息检索导论课件上面言多项式模型比伯努利模型准确度高 类条件概率P(tk|c)=(类c 下单词tk
- * 在各个文档中出现过的次数之和+1)/(类c下单词总数+|V|)
+ * 利用朴素贝叶斯算法新闻文档集做分类，采用十组交叉测试取平均值
+ * 采用多项式模型,stanford信息检索导论上面言多项式模型比伯努利模型准确度高
  * 
- * @author yangliu
- * @qq 772330184
- * @mail yang.liu@pku.edu.cn
+ * @author liuxueping
+ * @mail liuxueping@benguo.cn
  * 
  */
 public class NaiveBayesianClassifier {
+	private static final Logger log = LoggerFactory.getLogger(NaiveBayesianClassifier.class);
 	private static final DecimalFormat DF = new DecimalFormat("##.###");
 	private static int NUM_OF_CLS = 2;//分类个数
-	private static Map<String, String> clsName = new HashMap<String, String>();
 	private static TrainModel model = readModel("");
 	/**
 	 * 用贝叶斯法对测试文档集分类
 	 * 
-	 * @param trainDir
-	 *            训练文档集目录
-	 * @param testDir
-	 *            测试文档集目录
-	 * @param classifyResultFileNew
-	 *            分类结果文件路径
+	 * @param trainDir 训练文档集目录
+	 * @param testDir 测试文档集目录
+	 * @param classifyResultFileNew 分类结果文件路径
 	 * @throws Exception
 	 */
-	private void doProcess(String trainDir, String testDir, String classifyResultFileNew) throws Exception {
+	private void doProcess(String testDir, String classifyResultFileNew) throws Exception {
 		//下面开始读取测试样例做分类
 		Vector<String> testFileWords = new Vector<String>();
-		String word;
+		String word;BufferedReader reader = null;
 		File[] testDirFiles = new File(testDir).listFiles();
 		FileWriter crWriter = new FileWriter(classifyResultFileNew);
 		for (int i = 0; i < testDirFiles.length; i++) {
@@ -71,109 +55,63 @@ public class NaiveBayesianClassifier {
 			for (int j = 0; j < testSample.length; j++) {
 				testFileWords.clear();
 				//分词提取特征词
-				FileReader spReader = new FileReader(testSample[j]);
-				BufferedReader spBR = new BufferedReader(spReader);
-				while ((word = spBR.readLine()) != null) {
+				log.info("extract keywords for test content: {}", testSample[j].getAbsolutePath());
+				reader = new BufferedReader(new FileReader(testSample[j]));
+				while ((word = reader.readLine()) != null) {
 					testFileWords.add(word);
 				}
 				//下面分别计算该测试样例相对于各类别的概率
-				File[] trainDirFiles = new File(trainDir).listFiles();
+				log.info("compute probability for each class : {}", Constants.clsName);
 				BigDecimal maxP = new BigDecimal(0);
-				String bestCate = null;
-				for (int k = 0; k < trainDirFiles.length; k++) {
-					BigDecimal p = computeCateProb(trainDirFiles[k], testFileWords, model);
-					if (k == 0) {
+				String bestCate = null;int loop = -1;
+				for (String key : Constants.clsName.keySet()) {
+					loop ++;
+					BigDecimal p = computeCateProb(key, testFileWords, model);
+					if (loop == 0) {
 						maxP = p;
-						bestCate = trainDirFiles[k].getName();
+						bestCate = key;
 						continue;
 					}
 					if (p.compareTo(maxP) == 1) {
 						maxP = p;
-						bestCate = trainDirFiles[k].getName();
+						bestCate = key;
 					}
 				}
 				crWriter.append(testDirFiles[i].getName() + "_" + testSample[j].getName() + " " + bestCate + "\n");
-				crWriter.flush();
 			}
 		}
+		crWriter.flush();
+		reader.close();
 		crWriter.close();
 	}
 
-	/**
-	 * 统计某类训练样本中每个单词的出现次数
-	 * 
-	 * @param strDir
-	 *            训练样本集目录
-	 * @return Map<String,Double> cateWordsProb
-	 *         用"类目_单词"对来索引的map,保存的val就是该类目下该单词的出现次数
-	 * @throws IOException
-	 */
-	public Map<String, Double> getCateWordsProb(String strDir){
-		Map<String, Double> cateWordsProb = new TreeMap<String, Double>();
-		File sampleFile = new File(strDir);
-		File[] sampleDir = sampleFile.listFiles();
-		String word;BufferedReader reader = null;
-		try {
-			for (int i = 0; i < sampleDir.length; i++) {
-				File[] sample = sampleDir[i].listFiles();
-				for (int j = 0; j < sample.length; j++) {
-					reader = new BufferedReader(new FileReader(sample[j]));
-//				BufferedReader reader = IOUtil.getReader(sample[j].getAbsolutePath(), IOUtil.GBK);
-					while ((word = reader.readLine()) != null) {
-						String key = sampleDir[i].getName() + "_" + word;
-						if (cateWordsProb.containsKey(key)) {
-							double count = cateWordsProb.get(key) + 1.0;
-							cateWordsProb.put(key, count);
-						} else {
-							cateWordsProb.put(key, 1.0);
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally{
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return cateWordsProb;
-	}
 
 	/**
 	 * 计算某一个测试样本属于某个类别的概率
 	 * 
-	 * @param Map
-	 *            <String, Double> cateWordsProb 记录每个目录中出现的单词及次数
-	 * @param File
-	 *            trainFile 该类别所有的训练样本所在目录
-	 * @param Vector
-	 *            <String> testFileWords 该测试样本中的所有词构成的容器
+	 * @param Map <String, Double> cateWordsProb 记录每个目录中出现的单词及次数
+	 * @param File trainFile 该类别所有的训练样本所在目录
+	 * @param Vector <String> testFileWords 该测试样本中的所有词构成的容器
 	 * @param double totalWordsNum 记录所有训练样本的单词总数
-	 * @param Map
-	 *            <String, Double> cateWordsNum 记录每个类别的单词总数
+	 * @param Map <String, Double> cateWordsNum 记录每个类别的单词总数
 	 * @return BigDecimal 返回该测试样本在该类别中的概率
 	 * @throws Exception
 	 * @throws IOException
 	 */
-	private BigDecimal computeCateProb(File trainFile, Vector<String> testFileWords, TrainModel mode) throws Exception {
+	private BigDecimal computeCateProb(String clsName, Vector<String> testFileWords, TrainModel mode) throws Exception {
 		BigDecimal probability = new BigDecimal(1);
-		double wordNumInCate = mode.getCateWordsNum().get(trainFile.getName());
+		double wordNumInCate = mode.getCateWordsNum().get(clsName);
 		BigDecimal wordNumInCateBD = new BigDecimal(wordNumInCate);
 		BigDecimal totalWordsNumBD = new BigDecimal(mode.getTotalWordsNum());
-		Map<String, Double> cateWordsProb = mode.getCateWordsProb();
-		for (Iterator<String> it = testFileWords.iterator(); it.hasNext();) {
-			String me = it.next();
-			String key = trainFile.getName() + "_" + me;
+		Map<String, Map<String, Double>> cateWordsProb = mode.getCateWordsProb();
+		for (String me : testFileWords) {
 			double testFileWordNumInCate = 0.0;
-			if (cateWordsProb.containsKey(key)) {
-				testFileWordNumInCate = cateWordsProb.get(key);
+			if (cateWordsProb.containsKey(clsName) && cateWordsProb.get(clsName).containsKey(me)) {
+				testFileWordNumInCate = cateWordsProb.get(clsName).get(me);
 			}
 			BigDecimal testFileWordNumInCateBD = new BigDecimal(testFileWordNumInCate);
 			//多元分布模型( multinomial model )  –以单词为粒度
-			//原始计算：类条件概率P(tk|c)=(类c下单词tk在各个文档中出现过的次数之和+1)/（类c下单词总数+训练样本中不重复特征词总数）
+			//原始计算：类条件概率P(tk|c)=(类c下单词tk在各个文档中出现过的次数之和+1)/（类c下单词总数+训练样本中单词总数）
 			//优化改进：类条件概率P(tk|c)=(类c下单词tk在各个文档中出现过的次数之和+0.001)/（类c下主题词总数+训练样本中不重复特征词总数）
 			BigDecimal xcProb = (testFileWordNumInCateBD.add(new BigDecimal(0.001))).divide(totalWordsNumBD.add(wordNumInCateBD), 10, BigDecimal.ROUND_CEILING);
 			probability = probability.multiply(xcProb);
@@ -189,10 +127,8 @@ public class NaiveBayesianClassifier {
 	/**
 	 * 根据正确类目文件和分类结果文件统计出准确率
 	 * 
-	 * @param classifyResultFile
-	 *            正确类目文件
-	 * @param classifyResultFileNew
-	 *            分类结果文件
+	 * @param classifyResultFile 正确类目文件
+	 * @param classifyResultFileNew 分类结果文件
 	 * @return double 分类的准确率
 	 * @throws IOException
 	 */
@@ -213,10 +149,8 @@ public class NaiveBayesianClassifier {
 	/**
 	 * 根据正确类目文件和分类结果文计算混淆矩阵并且输出
 	 * 
-	 * @param rightCate
-	 *            正确类目对应map
-	 * @param resultCate
-	 *            分类结果对应map
+	 * @param rightCate  正确类目对应map
+	 * @param resultCate 分类结果对应map
 	 * @return double 分类的准确率
 	 * @throws IOException
 	 */
@@ -240,12 +174,12 @@ public class NaiveBayesianClassifier {
 		double[] hangSum = new double[NUM_OF_CLS];
 		System.out.print(format(""));
 		for (int i = 0; i < NUM_OF_CLS; i++) {
-			System.out.print(format(clsName.get(cateNamesArray[i])));
+			System.out.print(format(Constants.clsName.get(cateNamesArray[i])));
 		}
 		System.out.print(format("hit(%)"));
 		System.out.println(format("+"));
 		for (int i = 0; i < NUM_OF_CLS; i++) {
-			System.out.print(format(clsName.get(cateNamesArray[i])));
+			System.out.print(format(Constants.clsName.get(cateNamesArray[i])));
 			for (int j = 0; j < NUM_OF_CLS; j++) {
 				System.out.print(format(confusionMatrix[i][j]));
 				hangSum[i] += confusionMatrix[i][j];
@@ -258,23 +192,22 @@ public class NaiveBayesianClassifier {
 	/**
 	 * 从分类结果文件中读取map
 	 * 
-	 * @param classifyResultFileNew
-	 *            类目文件
+	 * @param classifyResultFileNew  类目文件
 	 * @return Map<String, String> 由<文件名，类目名>保存的map
 	 * @throws IOException
 	 */
 	private Map<String, String> getMapFromResultFile(String classifyResultFileNew) throws IOException {
 		// TODO Auto-generated method stub
 		File crFile = new File(classifyResultFileNew);
-		FileReader crReader = new FileReader(crFile);
-		BufferedReader crBR = new BufferedReader(crReader);
+		BufferedReader reader = new BufferedReader(new FileReader(crFile));
 		Map<String, String> res = new TreeMap<String, String>();
 		String[] s;
 		String line;
-		while ((line = crBR.readLine()) != null) {
+		while ((line = reader.readLine()) != null) {
 			s = line.split(" ");
 			res.put(s[0], s[1]);
 		}
+		reader.close();
 		return res;
 	}
 
@@ -285,21 +218,14 @@ public class NaiveBayesianClassifier {
 	public void NaiveBayesianClassifierMain(String[] args) throws Exception {
 		int verifyLoop = Constants.VERIFY_LOOP;
 		//首先创建训练集和测试集
-		BufferedReader reader = IOUtil.getReader("D:/testminezh/class.txt", IOUtil.GBK);
-		String cls = null;
-		while ((cls = reader.readLine()) != null) {
-			String[] clz = cls.split("\t");
-			clsName.put(clz[0], clz[1]);
-		}
 		NaiveBayesianClassifier nbClassifier = new NaiveBayesianClassifier();
 		double[] accuracyOfEveryExp = new double[10];
 		double accuracyAvg, sum = 0;
 		for (int i = 0; i < verifyLoop; i++) {//用交叉验证法做十次分类实验，对准确率取平均值	
-			String TrainDir = Constants.ROOT_DIR + "TrainSample" + i;
-			String TestDir = Constants.ROOT_DIR + "TestSample" + i;
+			String testDir = Constants.ROOT_DIR + "TestSample" + i;
 			String classifyRightCate = Constants.ROOT_DIR + "classifyRightCate" + i + ".txt";
 			String classifyResultFileNew = Constants.ROOT_DIR + "classifyResultNew" + i + ".txt";
-			nbClassifier.doProcess(TrainDir, TestDir, classifyResultFileNew);
+			nbClassifier.doProcess(testDir, classifyResultFileNew);
 			accuracyOfEveryExp[i] = nbClassifier.computeAccuracy(classifyRightCate, classifyResultFileNew);
 			System.out.println("The accuracy for Naive Bayesian Classifier in " + (i + 1) + "th Exp is :" + DF.format(accuracyOfEveryExp[i]) + "\n");
 		}
@@ -318,7 +244,7 @@ public class NaiveBayesianClassifier {
 	 */
 	static TrainModel readModel(String modelPath) {
 		if (StringUtil.isBlank(modelPath)) {
-			modelPath = "d:/clsmode";
+			modelPath = "d:/classify2.mod";
 		}
 		ObjectInputStream in = null;
 		try {
@@ -338,6 +264,11 @@ public class NaiveBayesianClassifier {
 		}
 	}
 
+	/**
+	 * 格式化输出
+	 * @param o
+	 * @return
+	 */
 	String format(Object o) {
 		int length = 10;
 		if ("+".equals(o.toString())) {
